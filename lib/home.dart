eb7b5213da3dcc4004de2bd8911b2cc29eb77126
Bibek bhattarai <bhattaraibvk@gmail.com>
2020-10-22 22:33:50 +0545

    second commit

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:drmobile/menu/prescription.dart';
 import 'package:flutter/material.dart';
 //import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -22,7 +23,9 @@ class _homeState extends State<home> {
             children: [
               Column(
                 children: [
+                  Icon(Icons.dry_cleaning),
                   Text("Diagnosis"),
+                  Icon(Icons.warning_amber_rounded),
                   Text("Sex Education"),
                   Text("Emergency No.")
                 ],
@@ -37,12 +40,25 @@ class _homeState extends State<home> {
               Column(
                 children: [
                   Text("Abortion"),
+                  InkWell(
+                    child: Icon(Icons.upload_rounded),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => sendPresccription()));
+                    },
+                  ),
                   Text("Send Prescription"),
                   Text("Help/About us")
                 ],
               )
             ],
           ),
+          SizedBox(
+              width: 300,
+              height: 5,
+              child: Container(color: Colors.green.shade800, height: 10)),
           Column(children: [
             Container(
                 decoration: BoxDecoration(
@@ -55,7 +71,76 @@ class _homeState extends State<home> {
                       fontSize: 22,
                       fontWeight: FontWeight.w800),
                 ))
-          ])
+          ]),
+
+          // Section of inviting is started
+          SizedBox(
+              width: 300,
+              height: 5,
+              child: Container(color: Colors.green.shade800, height: 10)),
+
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Column(children: [
+                  Container(
+                    height: 80,
+                    width: 60,
+                    child: Image.asset('images/dr.jpg'),
+                  ),
+                  Text('MBBS Doctor'),
+                  RaisedButton(child: Text("Invite"), onPressed: () {})
+                ]),
+                Column(children: [
+                  Container(
+                    height: 80,
+                    width: 60,
+                    child: Image.asset('images/dr.jpg'),
+                  ),
+                  Text('Health Assistance'),
+                  RaisedButton(child: Text("Invite"), onPressed: () {})
+                ]),
+                Column(children: [
+                  Container(
+                    height: 80,
+                    width: 60,
+                    child: Image.asset('images/dr.jpg'),
+                  ),
+                  Text('Staff Nurse'),
+                  RaisedButton(child: Text("Invite"), onPressed: () {})
+                ]),
+                Column(children: [
+                  Container(
+                    height: 80,
+                    width: 60,
+                    child: Image.asset('images/dr.jpg'),
+                  ),
+                  Text('Assitance Nurse Mid-wifery'),
+                  RaisedButton(child: Text("Invite"), onPressed: () {})
+                ]),
+                Column(children: [
+                  Container(
+                    decoration: BoxDecoration(),
+                    height: 80,
+                    width: 60,
+                    child: Image.asset('images/dr.jpg'),
+                  ),
+                  Text('CMA'),
+                  RaisedButton(child: Text("Invite"), onPressed: () {})
+                ]),
+              ],
+            ),
+          ),
+
+          // Section of inviting ends
+          Column(
+            children: [
+              Icon(Icons.warning_amber_outlined),
+              Text("Emergency??"),
+              Text("Our volunteer are here to help you")
+            ],
+          )
         ],
       ),
     );
---
+++ lib/menu/prescription.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class sendPresccription extends StatefulWidget {
+  @override
+  _sendPresccriptionState createState() => _sendPresccriptionState();
+}
+
+class _sendPresccriptionState extends State<sendPresccription> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
